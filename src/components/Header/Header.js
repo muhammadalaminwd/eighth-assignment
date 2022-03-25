ea2a1302e00fb171750e9245c7a860e5baffd291
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-03-25 22:58:14 +0600

    header-created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Ocean of Knowledge</h1>
+        </div>
+    );
+};
+
+export default Header;
