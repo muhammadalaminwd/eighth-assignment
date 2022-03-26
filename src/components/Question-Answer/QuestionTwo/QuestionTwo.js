03b76f1e436050e270a4d9e066b09e5c83090234
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-03-26 18:03:26 +0600

    question-answer-modified

--- src/components/Question-Answer/QuestionTwo/QuestionTwo.js
+++ src/components/Question-Answer/QuestionTwo/QuestionTwo.js
@@ -5,7 +5,13 @@ const QuestionTwo = () => {
         <div>
             <h1 className='question'>Q2: How useState Works?</h1>
             <p className='answer'>Ans: useState is a hook that allows us to have state variables in functional components. We can pass initial state to that function and can get returns value.<br></br>
-            It generates a single piece of state associated with that component. The state in a class is always an object, with hooks, the state can be any type. We can declare state in react by writing- React.useState or import it.</p>
+            It generates a single piece of state associated with that component. The state in a class is always an object, with hooks, the state can be any type. We can declare state in react by writing- React.useState or import it.<br></br>
+            Some important points to remember:<br></br>
+            i) Update function doesn't update the value directly.
+            ii) React won't trigger a re-render, if we use the same value as the current state.
+            iii) useState doesn't marge objects when the state is updated.
+            iv) It follows the same rules that all the hooks do.
+            </p>
         </div>
     );
 };
